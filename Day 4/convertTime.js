c9a49e3222062770211c621dfb64e9da5ddaa021
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-02-09 09:54:07 +0545

    day 4 added

---
+++ Day 4/convertTime.js
@@ -0,0 +1,28 @@
+// Write a function which can convert the time input given in 12 hours format to 24 hours format
+
+const time = '12:10AM';
+
+function convertTo24HrsFormat(time) {
+    // write your solution here
+    const median = time.slice(-2);
+    let [hours, minutes] = time.split(":")
+    if (hours < 12) {
+        let add_zero = "0"
+        hours = add_zero.concat(hours)
+    }
+    if (hours === "12") {
+        hours = "00"
+    }
+
+    if (median === "PM") {
+        hours = parseInt(hours) + 12
+    }
+    minutes = minutes.slice(0,-2);
+
+    if(minutes.length ===1){
+        minutes = '0'+ minutes
+    }
+    return hours + ':'+ minutes
+}
+
+console.log(`Converted time: ${convertTo24HrsFormat(time)}`)
